92ec75d67a2d72be6ed2ec15ce46cc1df42ca18e
Larry Devlin <larrydevlin1770@gmail.com>
2024-11-05 16:28:21 -0500

    Began working on prime gap challenge.

--- rc-006.lisp
+++ rc-006.lisp
@@ -1954,4 +1954,159 @@ Assume that the sequence may be very long.
 
 ;; My version returnx two values: the index 
 ;; and the sum.
-    
+
+#| Earliest Difference Between Prime Gaps
+
+When calculating prime numbers > 2, the
+difference between adjacent primes is always
+an even number. This difference, also referred
+to as the gap, varies in an random pattern; at
+least, no pattern has ever been discovered,
+and it is strongly conjectured that no pattern
+exists. However, it is also conjectured that
+between some two adjacent primes will be a
+gap corresponding to every positive even
+integer.
+
+This task involves locating the minimal primes 
+corresponding to those gaps.
+
+Though every gap value exists, they don't
+seem to come in any particular order.
+For example, this table shows the gaps and
+minimum starting value primes for 2 through
+30:
+
+gap 	msp* 	ep**
+
+2 	3 	5
+4 	7 	11
+6 	23 	29
+8 	89 	97
+10 	139 	149
+12 	199 	211
+14 	113 	127
+16 	1831 	1847
+18 	523 	541
+20 	887 	907
+22 	1129 	1151
+24 	1669 	1693
+26 	2477 	2503
+28 	2971 	2999
+30 	4297 	4327
+
+* msp: minimal starting prime
+** ep: ending prime
+
+For the purposes of this task, considering only primes greater than 2, consider prime gaps that differ by exactly two to be adjacent. 
+
+
+Task
+
+For each order of magnitude m from 10¹ through
+10⁶:
+
+  •  Find the first two sets of minimum adjacent
+     prime gaps where the absolute value of the
+     difference between the prime gap start
+     values is greater than m.
+
+
+E.G.
+
+For an m of 10¹;
+
+The start value of gap 2 is 3, the start value of
+gap 4 is 7, the difference is 7 - 3 or 4. 4 < 10¹
+so keep going.
+
+The start value of gap 4 is 7, the start value of
+gap 6 is 23, the difference is 23 - 7, or 16. 16 >
+10¹ so this the earliest adjacent gap
+difference > 10¹.
+
+Stretch goal
+
+  • Do the same for 10⁷ and 10⁸ (and higher?)
+    orders of magnitude
+
+Note: the earliest value found for each order
+of magnitude may not be unique, in fact, IS not
+unique ; also, with the gaps in ascending order,
+the minimal starting values are not strictly
+ascending.
+
+|#
+
+;; First, we build an in-place sieve.
+
+;; We can weed out 77% of the search space
+;; by not considering multiples of the first
+;; four primes in the input to the
+;; sequential prime finder. These multiples
+;; form a repetitive ring of distances between
+;; factors. The ring has a circumference of 210
+;; with 48 holes, the possible primes.
+
+(setf *print-circle* t)
+
+(defconstant +wheel+
+  '#1=(2 4 2 4 6 2 6 4 2 4 6 6
+       2 6 4 2 6 4 6 8 4 2 4 2
+       4 8 6 4 6 4 2 6 2 6 6 4
+       2 4 6 2 6 4 2 4 2 10 2 10
+       . #1#)
+  "Circular list containing the
+  wheel of differences between
+  integers not divisible by 2, 3,
+  5 or 7. The wheel starts at 11
+  and the first revolution ends
+  at 231.")
+
+;; To create an in-place sieve, we start
+;; with a collection containing (prime multiple)
+;; pairs. These are stored in a min-heap,
+;; keyed to the multiples. For each new
+;; number n  in the input, we compare it to
+;; the least multiple m1. If n < m1, then
+;; n is prime. It and its square (the multiple)
+;; are added to the heap. If not,
+;; is n == m1? If so, n is composite.
+;; Increment m1 by its corresponding
+;; prime. If not, n > m1. Increment m1
+;; by its corresponding prime. Then,
+;; take m2, the top composite of the heap
+;; after the reshuffling and try again.
+;; Continue this process until n <= the test
+;; composite, m[L].If n < m[L], n is
+;; prime. Othersise, it is composite.
+
+(defstruct (heap-entry
+	    (:conc-name nil)
+	    (:constructor entry (pri &optional com)))
+  (prime nil :type integer)
+  (comp nil :type (or integer null)))
+
+(defmethod initialize-instance :after ((entry heap-entry))
+  "When the composite member of the heap-entry 
+   has not been supplied, populate the slot
+   with the square of the prime slot value.
+   Return the instance."
+  (when (null (comp entry))
+    (let ((prime (prime entry)))
+      (setf (comp entry)
+	    (* prime prime))))
+  entry)
+
+(defmethod incf-entry ((entry heap-entry))
+  "Increment the composite value of the 
+   heap-entry by its prime value. Return
+   the entry"
+  (incf (comp entry)
+	(prime entry))
+  entry)
+
+
+
+
+
